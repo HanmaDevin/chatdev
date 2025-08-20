b90a76840358608f69123a78945410d6c853991d
HanmaDevin <devinbc14@gmail.com>
2025-08-20 20:54:05 +0200

    chages

--- main.go
+++ main.go
@@ -1,1 +1,26 @@
 package main
+
+import (
+	"time"
+
+	"github.com/HanmaDevin/chatdev/types"
+	"github.com/HanmaDevin/chatdev/views"
+	"github.com/labstack/echo"
+)
+
+func indexHandler(c echo.Context) error {
+	data := types.Index{
+		AppName:     "My Echo App",
+		CurrentTime: time.Now(),
+	}
+	component := views.Index(data)
+
+	return component.Render(c.Request().Context(), c.Response().Writer)
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", indexHandler)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
---
+++ types/index.go
@@ -0,0 +1,8 @@
+package types
+
+import "time"
+
+type Index struct {
+	AppName     string
+	CurrentTime time.Time
+}
